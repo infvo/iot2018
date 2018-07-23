6c5d285af371c3046340d88581cf5124359557e3
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2018-07-23 16:21:14 +0200

    Code beautification

--- LED-webserver-0/LED-webserver-0.ino
+++ LED-webserver-0/LED-webserver-0.ino
@@ -4,7 +4,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "...";            // adapt to local WiFi
+const char* ssid = "...";     // adapt to local WiFi
 const char* password = "..."; // adapt to local WiFi
 unsigned char mac[6];
 
